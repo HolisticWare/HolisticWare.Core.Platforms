5585c86830c57215a907671cbecb97a99b727646
moljac <mcvjetko@holisticware.net>
2016-02-26 10:32:03 +0100

    folder renaming

---
+++ source/HolisticWare.Core.Platforms.LinkSource/OperatingSystems/UnixLike.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Platforms
+{
+	/// <summary>
+	/// 
+	/// </summary>
+	/// <see cref="http://www.linfo.org/unix-like.html"/>
+	public abstract partial class UnixLike : OperatingSystem
+	{
+	}
+}
